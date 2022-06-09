43a0b87d32e1af6d6014242e2a9a47b62b519fd5
Cluster-Taek <yst811@naver.com>
2022-06-09 15:52:40 +0900

    feat : 404 redirect 설정

---
+++ src/pages/404.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import styles from '../../styles/Home.module.css'
+import Error from "next/error";
+
+export default function Custom404() {
+    const router = useRouter();
+    const [timer, setTimer] = useState(5);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (timer === 1) {
+                router.replace("/");
+            } else {
+                setTimer(t => t - 1)
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    })
+
+    return (
+        <div className={styles.container}>
+            <p>요청하신 페이지는 존재하지 않습니다.</p>
+            <p>{timer}초 후 메인페이지로 이동합니다.</p>
+        </div>
+    );
+}
